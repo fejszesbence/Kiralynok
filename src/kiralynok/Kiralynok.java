e111252f69a16cb7588081a8e4164a372ce0c3a4
fejszesbence <fejszesbence@gmail.com>
2021-02-18 10:22:15 +0100

    4. feladat kész

--- src/kiralynok/Kiralynok.java
+++ src/kiralynok/Kiralynok.java
@@ -5,6 +5,17 @@ import java.io.IOException;
 public class Kiralynok {
     public static void main(String[] args) throws IOException {
         
+        System.out.println("4. feladat");
+        Tabla tabla = new Tabla('#');
+        tabla.megjelenit();
+        
+        
+        
+        System.out.println("6. feladat");
+        
+        
+        
+        
     }
     
 }
